a24a9d676fbf655f655d7858c3c5b94dae48eb4a
mahosAtpug <68770124+mahosAtpug@users.noreply.github.com>
2021-03-23 20:19:36 +0530

    Add files via upload

---
+++ screens/homeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {TouchableOpacity , Text , Image , StyleSheet , View, TextInput, Alert , Modal, ScrollView , KeyboardAvoidingView} from "react-native"
+
+export default class HomeScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Home
+                </Text>
+            </View>
+        )
+    }
+}
